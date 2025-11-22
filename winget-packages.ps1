73f8faac8aa1ebc17e719d634391636d4cfd43c9
Andrew Herendeen <andrew@aherendeen.com>
2025-11-22 04:33:32 -0700

    Create winget-packages.ps1 fAdd winget-packages.ps1 script for Windows package installation using wingetor package installation
    This script installs a list of Windows packages using winget.

---
+++ winget-packages.ps1
@@ -0,0 +1,23 @@
+# Windows packages installation using winget
+# Run this script in an elevated PowerShell session.
+
+$packages = @(
+    "Git.Git",
+    "Microsoft.WindowsTerminal",
+    "JanDeDobbeleer.OhMyPosh",
+    "Starship.Starship",
+    "Neovim.Neovim",
+    "junegunn.fzf",
+    "sharkdp.bat",
+    "BurntSushi.ripgrep",
+    "7zip.7zip",
+    "Python.Python.3",
+    "OpenJS.NodeJS"
+)
+
+foreach ($pkg in $packages) {
+    winget install --id=$pkg -e --source winget
+}
+
+# Install chezmoi
+winget install --id=twpayne.chezmoi -e --source winget
